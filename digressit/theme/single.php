e0ba63a6c0bd88bd29f4538ad4b4aa15ea5b31d3
eddie.tejeda <eddie.tejeda@b80beb6a-a484-11de-b98f-4500b42dc123>
2009-09-28 23:15:58 +0000

    2.6 compatability.. checks for non-existant post_class function
    git-svn-id: https://digressit.googlecode.com/svn/trunk@6 b80beb6a-a484-11de-b98f-4500b42dc123

--- digressit/theme/single.php
+++ digressit/theme/single.php
@@ -18,7 +18,7 @@ get_header();
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+		<div <?php if(function_exists('post_class')){ post_class(); } ?> id="post-<?php the_ID(); ?>">
 			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<div class="entry">
 				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
