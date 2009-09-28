3914edf2be0810aeb3366aab1d0f825fdc368816
eddie.tejeda <eddie.tejeda@b80beb6a-a484-11de-b98f-4500b42dc123>
2009-09-28 23:18:18 +0000

    2.6 compatabiluty: checks for post_password_required
    git-svn-id: https://digressit.googlecode.com/svn/trunk@7 b80beb6a-a484-11de-b98f-4500b42dc123

--- digressit/theme/comments.php
+++ digressit/theme/comments.php
@@ -8,7 +8,8 @@
 	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
 		die ('Please do not load this page directly. Thanks!');
 
-	if ( post_password_required() ) { ?>
+
+	if ( !function_exists('post_password_required') && post_password_required() ) { ?>
 		<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
 	<?php
 		return;
