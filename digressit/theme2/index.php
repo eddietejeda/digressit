9e2c703f5f1004f9b675c5baa0e013391b42dbaf
alex.bilbie <alex.bilbie@b80beb6a-a484-11de-b98f-4500b42dc123>
2009-11-19 15:16:18 +0000

    Added a new theme folder that will (hopefully) become the new theme. Testing of this theme will be on http://jiscpress.org/
    git-svn-id: https://digressit.googlecode.com/svn/trunk@56 b80beb6a-a484-11de-b98f-4500b42dc123

---
+++ digressit/theme2/index.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+
+get_header();
+
+global $digressit_commentbrowser;
+$options = get_option('digressit');
+
+
+
+?>
+
+	<div class="frontpage">
+
+		<div id="leftcolumn"> 
+
+			<h2>Table of Contents</h2>
+			<ol>
+			<?php
+			//global $post;
+
+			extract($options);
+
+			$myposts = null;
+			if($front_page_post_type){
+				$myposts = get_posts("post_type=$front_page_post_type&numberposts=-1&order=$front_page_order&orderby=$front_page_order_by");
+			}
+			else{
+				$myposts = get_posts();				
+			}
+			
+			foreach($myposts as $post) :
+			?>
+	
+				<?php $comment_array = get_approved_comments($post->ID);  ?>
+
+				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?> (<?php echo count($comment_array); ?>)</a></li>
+			<?php endforeach; ?>
+
+			</ol> 
+
+
+		</div>
+		
+		<div id="middlecolumn">
+
+			<div id="blurb">
+
+			<?php 	
+
+				if($front_page_content != ''){
+
+					$page = get_post($front_page_content);
+       				$content = $page->post_content;
+       				$content = apply_filters('the_content', $content);
+
+					echo $content;
+				}
+				else{
+
+					$pages = get_pages(); 					
+						foreach($pages as $key=>$page){ 
+							if( $page->post_name == 'about'){
+		        				$content = $page->post_content;
+		        				$content = force_balance_tags(apply_filters('the_content', $content));
+								echo $content;
+								break;
+							}
+						}
+				}
+				if(is_admin()){
+					echo "<a href=''>Edit this text</a>";
+				}
+				
+			?>
+			</div>
+
+		
+		</div>
+
+		<div id="rightcolumn">
+			<?php get_sidebar(); ?>
+		</div>
+		
+	</div>
+
+
+<?php get_footer(); ?>
