ecba516b1cbfbb078c0462849728ea99e8d6f1f1
eddie.tejeda <eddie.tejeda@b80beb6a-a484-11de-b98f-4500b42dc123>
2010-11-17 18:58:27 +0000

    moving function files outside of theme
    git-svn-id: https://digressit.googlecode.com/svn/trunk@125 b80beb6a-a484-11de-b98f-4500b42dc123

--- core/single-functions.php
+++ core/single-functions.php
@@ -1,12 +1,72 @@
 <?php
 
-add_filter('wp', 'single_load');
 add_filter('init', 'single_init');
+add_action('wp_print_styles', 'digressit_single_print_styles');
+add_action('wp_print_scripts', 'digressit_single_print_scripts' );
+add_action('add_dynamic_widget', 'digressit_single_sidebar_widgets');
+
+
 
 
 function single_init(){
 	add_action('public_ajax_function', 'live_post_search_ajax');	
 }
+
+
+
+function digressit_single_print_styles(){
+	if(is_single()){
+	?>
+	<link rel="stylesheet" href="<?php echo get_digressit_media_uri('css/single.css'); ?>" type="text/css" media="screen" />
+	<?php
+	}
+}
+
+function digressit_single_print_scripts(){
+	if(is_single()){
+	wp_enqueue_script('digressit.single', get_digressit_media_uri('js/digressit.single.js'), 'jquery', false, true );		
+	}
+}
+
+
+function digressit_single_sidebar_widgets(){
+	if(is_single()){	
+		$options = get_option('digressit');
+		//var_dump(is_active_sidebar('Single Sidebar'));
+		if(is_active_sidebar('single-sidebar') && $options['enable_sidebar'] != 0){
+			?>
+			<div class="sidebar-widgets">
+			<div id="dynamic-sidebar" class="sidebar  <?php echo $options['auto_hide_sidebar']; ?> <?php echo $options['sidebar_position']; ?>">		
+			<?php
+			dynamic_sidebar('Single Sidebar');
+			?>
+			</div>
+			</div>
+			<?php
+		}
+	}		
+}
+
+
+/** 
+ * @description: 
+ * @todo: 
+ *
+ */
+function get_text_signature_count($post_ID, $text_signature)
+{
+	global $wpdb;
+	
+	$comments = $wpdb->get_results($wpdb->prepare("SELECT * FROM $wpdb->comments WHERE 	comment_approved = 1 AND comment_post_ID = %d", $post_ID) );
+	$comment_count = count(get_text_signature_filter($comments, $text_signature));
+	return $comment_count; //( $comment_count > 0) ? $comment_count : '';	
+}
+
+
+
+
+
+
 
 
 function live_post_search_ajax($request_params){
@@ -45,67 +105,6 @@ function live_post_search_ajax($request_params){
 }
 
 
-function single_wp_print_styles(){
-?>
-<link rel="stylesheet" href="<?php echo get_digressit_media_uri(); ?>/single.css" type="text/css" media="screen" />
-<?php
-}
-
-function single_wp_print_scripts(){
-	wp_enqueue_script('digressit.single', get_digressit_media_uri().'/single.js', 'jquery', false, true );		
-}
-
-
-function single_sidebar_widgets(){
-	
-	$options = get_option('digressit');
-	//var_dump(is_active_sidebar('Single Sidebar'));
-	if(is_active_sidebar('single-sidebar')){
-		?>
-		<div class="sidebar-widgets">
-		<div id="dynamic-sidebar" class="sidebar  <?php echo $options['auto_hide_sidebar']; ?> <?php echo $options['sidebar_position']; ?>">		
-		<?php
-		dynamic_sidebar('Single Sidebar');
-		?>
-		</div>
-		</div>
-		<?php
-	}
-}
-
-function single_load(){
-	if(is_single()){
-		add_action('wp_print_styles', 'single_wp_print_styles');
-		add_action('wp_print_scripts', 'single_wp_print_scripts' );
-		add_action('add_dynamic_widget', 'single_sidebar_widgets');
-	}
-}
-
-
-/** 
- * @description: 
- * @todo: 
- *
- */
-function get_text_signature_count($post_ID, $text_signature)
-{
-	global $wpdb;
-	
-	$comments = $wpdb->get_results($wpdb->prepare("SELECT * FROM $wpdb->comments WHERE 	comment_approved = 1 AND comment_post_ID = %d", $post_ID) );
-	$comment_count = count(get_text_signature_filter($comments, $text_signature));
-	return $comment_count; //( $comment_count > 0) ? $comment_count : '';	
-}
-
-
-
-
-
-function on_wp_head(){
-?>
-<meta http-equiv="X-UA-Compatible" content="chrome=1">
-<?php
-
-}
 
 
 ?>
